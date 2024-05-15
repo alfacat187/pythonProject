abc782b65719944241950dbaaebd1ffe86f862af
alexander <alfacat187@gmail.com>
2024-05-15 23:33:09 +0300

    sr commit

--- webhook.py
+++ webhook.py
@@ -19,7 +19,7 @@ TOKEN = '6677077782:AAEEiomVJtt1JnNntOjTjhR4_0pyoB1kzM0'
 
 # Webserver settings
 # bind localhost only to prevent any external access
-WEB_SERVER_HOST = "127.0.0.1"
+WEB_SERVER_HOST = "0.0.0.0"
 # Port for incoming request from reverse proxy. Should be any available port
 WEB_SERVER_PORT = 8080
 
